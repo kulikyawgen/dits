cc061427fc1b2b6ecc6204ad02f7f2105a1e1a9c
pomahcku <rom2517pop@gmail.com>
2020-10-11 18:55:44 +0300

    Merge roman_dev with master.

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+//This controller for testing your services. You can change it how you need.
+
 @Controller
 @RequestMapping("/")
 public class MainController {
